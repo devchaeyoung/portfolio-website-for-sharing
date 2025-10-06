8d99bf8a3390cdaeb958d57abb3ef0b2706226b0
devchaeyoung <chaemaa00@gmail.com>
2025-10-06 17:26:23 +0900

    Docs: edit validated function codeline

--- front/src/components/user/LoginForm.js
+++ front/src/components/user/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Container, Col, Row, Form, Button, Image } from 'react-bootstrap'
 import ResetPasswordModal from './ResetPasswordModal'
@@ -7,10 +7,30 @@ import * as Api from '../../api'
 import { DispatchContext } from '../../App'
 import { LoadingStateContext } from '../mainRouterComponent/MainRouterComponent'
 
+// email 형태 검사
+export const validateEmail = email => {
+  return email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+}
+
+//위 validateEmail 함수를 통해 이메일 형태 적합 여부를 확인함.
+export const isEmailValid = validateEmail(email)
+
+// 비밀번호가 4글자 이상인지 여부를 확인함.
+export const isPasswordValid = password.length >= 4
+
+// 이메일과 비밀번호 조건이 동시에 만족되는지 확인함.
+export const isFormValid = isEmailValid && isPasswordValid
+
 function LoginForm() {
+  const navigate = useNavigate()
+
   const [resetPasswordModalOn, setResetPasswordMadalOn] = useState(false)
   const [registerdModalOn, setRegisterMadalOn] = useState(false)
-  const navigate = useNavigate()
+
   const dispatch = useContext(DispatchContext)
   const { isFetchCompleted, setIsFetchCompleted } =
     useContext(LoadingStateContext)
@@ -20,22 +40,11 @@ function LoginForm() {
   //useState로 password 상태를 생성함.
   const [password, setPassword] = useState('')
 
-  //이메일이 abc@example.com 형태인지 regex를 이용해 확인함.
-  const validateEmail = email => {
-    return email
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      )
+  const handleLogin = async e => {
+    e.preventDefault()
+    try {
+    } catch (e) {}
   }
-
-  //위 validateEmail 함수를 통해 이메일 형태 적합 여부를 확인함.
-  const isEmailValid = validateEmail(email)
-  // 비밀번호가 4글자 이상인지 여부를 확인함.
-  const isPasswordValid = password.length >= 4
-  //
-  // 이메일과 비밀번호 조건이 동시에 만족되는지 확인함.
-  const isFormValid = isEmailValid && isPasswordValid
 
   const handleSubmit = async e => {
     e.preventDefault()
